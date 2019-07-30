b8e95c4962d0acc340f45bc5d58185514569f6c3
CallumReynolds <45584439+CallumReynolds@users.noreply.github.com>
2019-07-30 16:48:50 +1000

    init commit

---
+++ index.js
@@ -0,0 +1,101 @@
+
+
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+  var buttonInnerHTML = this.innerHTLML;
+
+  makeSound(buttonInnerHTML);
+
+  buttonAnimation(buttonInnerHTML);
+
+  });
+
+}
+
+document.addEventListener("keypress", function(event) {
+
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
+
+});
+
+function macleanSound() {
+  var mac = new Audio("sounds/macleansSoul.mp3");
+  mac.play();
+
+  var y = document.getElementById("w");
+
+  y.classList.add("pressed");
+  setTimeout(function() {
+    y.classList.remove("pressed");
+  }, 100);
+}
+
+function jordanSound() {
+  var jor = new Audio("sounds/jordanWhat.mp3");
+  jor.play();
+
+  var x = document.getElementById("a");
+
+  x.classList.add("pressed");
+  setTimeout(function() {
+    x.classList.remove("pressed");
+  }, 100);
+}
+
+
+function makeSound(key){
+
+  switch (key) {
+  case "w":
+    var mac = new Audio("sounds/macleansSoul.mp3");
+    mac.play();
+    break;
+
+  case "a":
+    var jor = new Audio("sounds/jordanWhat.mp3");
+    jor.play();
+    break;
+
+  case "s":
+    var tom3 = new Audio("sounds/tom-3.mp3");
+    tom3.play();
+    break;
+
+  case "d":
+    var tom4 = new Audio("sounds/tom-4.mp3");
+    tom4.play();
+    break;
+
+  case "j":
+    var snare = new Audio("sounds/snare.mp3");
+    snare.play();
+    break;
+
+  case "k":
+    var crash = new Audio("sounds/crash.mp3");
+    crash.play();
+    break;
+
+  case "l":
+    var kick = new Audio("sounds/kick-bass.mp3");
+    kick.play();
+    break;
+
+  default:
+    console.log(buttonInnerHTML);
+    }
+}
+
+function buttonAnimation(currentKey) {
+
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+
+}
